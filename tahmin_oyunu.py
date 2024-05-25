55be3523950953175e4aed8a323f22e61d77ebb3
Eren ÖZTÜRK <28493747+PourLa@users.noreply.github.com>
2024-05-25 21:42:58 +0300

    Update tahmin_oyunu.py

--- tahmin_oyunu.py
+++ tahmin_oyunu.py
@@ -2,35 +2,25 @@ import random as rd
 
 _author_ = "PourLa"
 print("TAHMİN ETME OYUNU")
-pc=rd.randrange(0,100)
+
+pc = rd.randrange(0, 100)
 hak = 0
 
 while True:
- try:
+    try:
+        tahmin = int(input("0-100 arasında bir sayı giriniz: "))
+        hak += 1
 
-   while True:
-    tahmin=int(input("0-100 arasında bir sayı giriniz:"))
-    hak += 1
+        if pc == tahmin:
+            print("Tam isabet, Tahmininiz Doğru!")
+            print("Kullanılan hak: {}".format(hak))
+            break
+        elif pc < tahmin:
+            print("Daha küçük bir sayı giriniz.")
+        elif pc > tahmin:
+            print("Daha büyük bir değer giriniz.")
 
-    if pc==tahmin :
-       print("Tam isabet, Tahmininiz Doğru")
-       print("Kullanılan hak : {}".format(hak))
-       input("ENTER'a basınız!")
-       break
+        print("Kullanılan hak: {}".format(hak))
 
-    elif pc<tahmin :
-       print("Daha küçük bir sayı giriniz")
-       print ("Kullanılan hak : {}".format (hak))
-
-    elif pc>tahmin :
-       print("Daha büyük bir değer giriniz")
-       print ("Kullanılan hak : {}".format (hak))
-
-    else :
-      print("Lütfen bir Sayı giriniz!")
-      print ("Kullanılan hak : {}".format (hak))
-
-   break
-
- except ValueError:
-    print("Lütfen sadece sayı girin!")
+    except ValueError:
+        print("Lütfen sadece sayı girin!")
